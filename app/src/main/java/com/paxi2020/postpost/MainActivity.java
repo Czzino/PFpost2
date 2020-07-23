816ff2d1e096cb6f4fb29061d43dfa75ae3c5082
fvwlharu <fvwlharu@naver.com>
2020-07-23 18:08:31 +0900

    cc

--- app/src/main/java/com/paxi2020/postpost/MainActivity.java
+++ app/src/main/java/com/paxi2020/postpost/MainActivity.java
@@ -28,8 +28,12 @@ public class MainActivity extends AppCompatActivity {
     ViewPager viewPager;
     DrawerLayout drawerLayout;
     TabAdapter adapter;
-
     NavigationView navi;
+
+    BottomNavigationView bnv;
+
+    FragmentManager fragmentManager;
+    Fragment[] fragments = new Fragment[4];
 
     ActionBarDrawerToggle drawerToggle;
 
@@ -38,6 +42,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+
+        fragments[0] = new PostFragment1();
+        fragments[1] = new PostFragment2();
+        fragments[2] = new PostFragment3();
+        fragments[3] = new PostFragment4();
+
+
+
+        FragmentTransaction tran = fragmentManager.beginTransaction();
+        tran.add(R.id.frame_container, fragments[0]);
+        tran.commit();
+
+        bnv = findViewById(R.id.bottom_nav);
+        bnv.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                //프레그먼트 작업 트랜잭션 시작
+                FragmentTransaction tran = fragmentManager.beginTransaction();
+
+                switch (menuItem.getItemId()) {
+                    case R.id.menu_lookpost:
+                        tran.replace(R.id.frame_container, fragments[0]);
+                        break;
+                    case R.id.menu_searchpost:
+                        tran.replace(R.id.frame_container, fragments[1]);
+                        break;
+                    case R.id.menu_shopping:
+                        tran.replace(R.id.frame_container, fragments[0]);
+                        break;
+                    case R.id.menu_booking:
+                        tran.replace(R.id.frame_container, fragments[1]);
+                        break;
+
+
+                }
+
+                //트랜잭션 작업 완료
+                tran.commit();
+
+                //return true가 아니면 ㅇ선택은 되지만 선택효과가 이동하지 않음
+                return true;
+            }
+        });
 
         navi=findViewById(R.id.nav);
         navi.setItemIconTintList(null);
